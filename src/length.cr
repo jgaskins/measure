140909a1d247b83d3ca9179857459e4c2efe288e
Jamie Gaskins <jgaskins@hey.com>
2025-02-19 11:30:14 -0600

    Improve method documentation

--- src/length.cr
+++ src/length.cr
@@ -5,47 +5,100 @@ module Measure
     include Comparable(self)
     include JSON::Serializable
 
+    # The numeric part of the measurement — the `1` in `1.meter`.
     getter magnitude : Float64
+
+    # The unit used in measuring the length.
     getter unit : Unit
 
+    # Instantiate a `Length` instance with the given `magnitude` and `Unit`.
     def initialize(@magnitude, @unit)
     end
 
-    def total_meters
+    # Returns the number of meters represented by this `Length` instance
+    def total_meters : Float64
       magnitude / coefficient
     end
 
-    def to_s(io) : Nil
+    # Output a human-readable representation of this `Length` to the given `IO`.
+    def to_s(io : IO) : Nil
       io << magnitude << ' '
       unit.to_s.downcase io
     end
 
+    # Convert this instance to the given `Unit`.
+    #
+    # ```
+    # 1.mile.to(:feet)
+    # # => Measure::Length(@magnitude=5280.0, @unit=Measure::Length::Unit::Foot)
+    # ```
     def to(unit : Unit) : self
       coefficient = coefficient(unit) / coefficient(self.unit)
       self.class.new(coefficient * magnitude, unit)
     end
 
+    # Add two `Length`s of any units together, returning an instance using
+    # `self`'s `Unit`.
+    #
+    # ```
+    # 1.kilometer + 1.mile
+    # # => Measure::Length(@magnitude=2.6093439485009937, @unit=Measure::Length::Unit::Kilometer)
+    # ```
     def +(other : self) : self
       self.class.new(magnitude + other.to(unit).magnitude, unit)
     end
 
+    # Subtract a `Length` from `self`, returning an instance using `self`'s
+    # `Unit`.
+    #
+    # ```
+    # 1.mile - 1.kilometer
+    # # => Measure::Length(@magnitude=0.37862878787878795, @unit=Measure::Length::Unit::Mile)
+    # ```
     def -(other : self) : self
       self.class.new(magnitude - other.to(unit).magnitude, unit)
     end
 
+    # Multiply by a scalar value
+    #
+    # ```
+    # 2.miles * 5
+    # # => Measure::Length(@magnitude=10.0, @unit=Measure::Length::Unit::Mile)
+    # ```
     def *(scalar : Number) : self
       self.class.new(magnitude * scalar, unit)
     end
 
+    # Multiply by a scalar value
+    #
+    # ```
+    # 10.miles / 2
+    # # => Measure::Length(@magnitude=5.0, @unit=Measure::Length::Unit::Mile)
+    # ```
     def /(scalar : Number) : self
       raise DivisionByZeroError.new if scalar == 0
       self.class.new(magnitude / scalar, unit)
     end
 
+    # Returns `-1` if `self` is less than `other`, `0` if they're equal, or `-1`
+    # otherwise.
+    #
+    # ```
+    # 1.mile <=> 1.kilometer # => 1
+    # 1.mile <=> 1.mile      # => 0
+    # 1.kilometer <=> 1.mile # => -1
+    # 1.kilometer < 1.mile   # => true
+    # 1.kilometer > 1.mile   # => false
+    # ```
     def <=>(other : self)
       total_meters <=> other.total_meters
     end
 
+    # Returns `true` if `self` and `other` are close enough to each other to be
+    # considered equivalent — within a femtometer (1/1_000_000_000_000_000th of
+    # a meter). This isn't _technically_ correct, but if you need that level of
+    # precision, [open an issue](https://github.com/jgaskins/measure/issues/new)
+    # and we can discuss how to support it.
     def ==(other : self)
       # If it's within a femtometer, we can call it close enough
       (total_meters - other.total_meters).abs < 1e-15
@@ -57,7 +110,7 @@ module Measure
         1
       in .kilometer?, .kilometers?, .km?
         0.001
-      in .centimeter?, .centimeters? , .cm?
+      in .centimeter?, .centimeters?, .cm?
         100
       in .millimeter?, .millimeters?, .mm?
         1_000
@@ -65,11 +118,11 @@ module Measure
         1_000_000
       in .nanometer?, .nanometers?, .nm?
         1_000_000_000
-      in .foot?, .feet?
+      in .foot?, .feet?, .ft?
         FEET_PER_METER
-      in .yard?, .yards?
+      in .yard?, .yards?, .yd?, .yds?
         FEET_PER_METER / 3
-      in .inch?, .inches?
+      in .inch?, .inches?, .in?
         FEET_PER_METER * 12
       in .mile?, .miles?, .mi?
         FEET_PER_METER / 5280
@@ -78,6 +131,9 @@ module Measure
 
     private FEET_PER_METER = 3.28084
 
+    # The units available to `Length` instances. Note that there are multiple
+    # aliases of each one. This allows you to say `length.to(:meters)` or
+    # `length.to(:km)`.
     enum Unit
       Kilometer
       Kilometers = Kilometer
@@ -105,12 +161,16 @@ module Measure
 
       Foot
       Feet = Foot
+      Ft   = Foot
 
       Yard
       Yards = Yard
+      Yd    = Yard
+      Yds   = Yard
 
       Inch
       Inches = Inch
+      In     = Inch
 
       Mile
       Miles = Mile
@@ -121,8 +181,14 @@ end
 
 struct Number
   {% for unit in Measure::Length::Unit.constants %}
-    def {{unit.downcase.id}}
-      Measure::Length.new(to_f64, :{{unit.downcase.id}})
+    # Instantiate a `Measure::Length` of `self` {{unit.downcase.id}}
+    #
+    # ```
+    # 1.{{unit.underscore.id}}
+    # # => Measure::Length(@magnitude=1.0, @unit=Measure::Length::Unit::{{unit}})
+    # ```
+    def {{unit.underscore.id}}
+      Measure::Length.new(to_f64, :{{unit.underscore.id}})
     end
   {% end %}
 end
--- src/weight.cr
+++ src/weight.cr
@@ -2,47 +2,100 @@ module Measure
   struct Weight
     include Comparable(self)
 
+    # The numeric part of the measurement — the `1` in `1.kilogram`.
     getter magnitude : Float64
+
+    # The unit used in measuring the weight.
     getter unit : Unit
 
+    # Instantiate a `{{@type}}` instance with the given `magnitude` and `Unit`.
     def initialize(@magnitude, @unit)
     end
 
+    # Returns the number of grams represented by this `{{@type}}` instance
     def total_grams
       magnitude / coefficient
     end
 
+    # Output a human-readable representation of this `{{@type}}` to the given `IO`.
     def to_s(io) : Nil
       io << magnitude << ' '
       unit.to_s.downcase io
     end
 
+    # Convert this instance to the given `Unit`.
+    #
+    # ```
+    # 1.kilogram.to(:lbs)
+    # # => Measure::Weight(@magnitude=2.20462, @unit=Measure::Weight::Unit::Pound)
+    # ```
     def to(unit : Unit) : self
       coefficient = coefficient(unit) / coefficient(self.unit)
       self.class.new(coefficient * magnitude, unit)
     end
 
+    # Add two `{{@type}}`s of any units together, returning an instance using
+    # `self`'s `Unit`.
+    #
+    # ```
+    # 1.kilogram + 1.pound
+    # # => Measure::Weight(@magnitude=1.4535929094356397, @unit=Measure::Weight::Unit::Kilogram)
+    # ```
     def +(other : self) : self
       self.class.new(magnitude + other.to(unit).magnitude, unit)
     end
 
+    # Subtract a `Length` from `self`, returning an instance using `self`'s
+    # `Unit`.
+    #
+    # ```
+    # 1.kilogram - 1.pound
+    # # => Measure::Weight(@magnitude=0.5464070905643603, @unit=Measure::Weight::Unit::Kilogram)
+    # ```
     def -(other : self) : self
       self.class.new(magnitude - other.to(unit).magnitude, unit)
     end
 
+    # Multiply by a scalar value
+    #
+    # ```
+    # 2.kg * 5
+    # # => Measure::Length(@magnitude=10.0, @unit=Measure::Length::Unit::Kilogram)
+    # ```
     def *(scalar : Number) : self
       self.class.new(magnitude * scalar, unit)
     end
 
+    # Multiply by a scalar value
+    #
+    # ```
+    # 10.kg / 2
+    # # => Measure::Length(@magnitude=2.0, @unit=Measure::Length::Unit::Kilogram)
+    # ```
     def /(scalar : Number) : self
       raise DivisionByZeroError.new if scalar == 0
       self.class.new(magnitude / scalar, unit)
     end
 
+    # Returns `-1` if `self` is less than `other`, `0` if they're equal, or `-1`
+    # otherwise.
+    #
+    # ```
+    # 1.kg <=> 1.lb # => 1
+    # 1.lb <=> 1.lb # => 0
+    # 1.lb <=> 1.kg # => -1
+    # 1.lb < 1.kg   # => true
+    # 1.lb > 1.kg   # => false
+    # ```
     def <=>(other : self)
       total_grams <=> other.total_grams
     end
 
+    # Returns `true` if `self` and `other` are close enough to each other to be
+    # considered equivalent — within a femtogram (1/1_000_000_000_000_000th of
+    # a gram). This isn't _technically_ correct, but if you need that level of
+    # precision, [open an issue](https://github.com/jgaskins/measure/issues/new)
+    # and we can discuss how to support it.
     def ==(other : self)
       # If it's within a femtogram, we can call it close enough
       (total_grams - other.total_grams).abs < 1e-15
@@ -50,13 +103,13 @@ module Measure
 
     private def coefficient(unit : Unit = self.unit)
       case unit
-      in .gram?, .grams?
+      in .gram?, .grams?, .g?
         1
       in .kilogram?, .kilograms?, .kg?
         1 / 1_000
       in .milligram?, .milligrams?, .mg?
         1_000
-      in .pound?, .pounds?, .lb?
+      in .pound?, .pounds?, .lb?, .lbs?
         POUNDS_PER_GRAM
       in .ounce?, .ounces?, .oz?
         POUNDS_PER_GRAM * 16
@@ -67,24 +120,34 @@ module Measure
 
     private POUNDS_PER_GRAM = 0.00220462
 
+    # The units available to `Weight` instances. Note that there are multiple
+    # aliases of each one. This allows you to say `weight.to(:pounds)` or
+    # `weight.to(:kilograms)`.
     enum Unit
       Gram
-      Grams      = Gram
+      Grams = Gram
+      G     = Gram
+
       Kilogram
-      Kilograms  = Kilogram
-      KG         = Kilogram
+      Kilograms = Kilogram
+      KG        = Kilogram
+
       Milligram
       Milligrams = Milligram
       MG         = Milligram
+
       Pound
-      Pounds     = Pound
-      LB         = Pound
+      Pounds = Pound
+      LB     = Pound
+      LBS    = Pound
+
       Ounce
-      Ounces     = Ounce
-      OZ         = Ounce
+      Ounces = Ounce
+      OZ     = Ounce
+
       Ton
-      Tons       = Ton
-      T          = Ton
+      Tons = Ton
+      T    = Ton
     end
   end
 end
